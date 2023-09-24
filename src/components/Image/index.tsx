2564bd7c2aa5e8330cccf73bd873365533d24eac
Benedict Umeozor <137875050+BenedictUmeozor@users.noreply.github.com>
2023-09-24 05:45:35 +0100

    Removed intersection observer

--- src/components/Image/index.tsx
+++ src/components/Image/index.tsx
@@ -1,5 +1,3 @@
-import { useRef, useEffect, useState } from "react";
-
 type PropTypes = {
   src: string;
   alt: string;
@@ -7,35 +5,10 @@ type PropTypes = {
 };
 
 const Image = ({ src, alt, className }: PropTypes) => {
-  const imgRef = useRef(null);
-  const [isVisible, setIsVisible] = useState(false);
-
-  useEffect(() => {
-    const observer = new IntersectionObserver((entries) => {
-      const [entry] = entries;
-      if (entry.isIntersecting) {
-        setIsVisible(true);
-        observer.unobserve(entry.target);
-      }
-    });
-
-    if (imgRef.current) {
-      observer.observe(imgRef.current);
-    }
-
-    const currentImgRef = imgRef.current;
-
-    return () => {
-      if (currentImgRef) {
-        observer.unobserve(currentImgRef);
-      }
-    };
-  }, []);
 
   return (
     <img
-      ref={imgRef}
-      src={isVisible ? src : ""}
+      src={src}
       alt={alt}
       className={className}
     />
